3bbafae2215eb90113d09bceee9292b70c0298e1
99003520 <78562135+99003520@users.noreply.github.com>
2021-02-05 17:22:53 +0530

    Update per.c

--- per.c
+++ per.c
@@ -10,7 +10,7 @@ int main()
     printf("Enter 2 numbers: \n");
     scanf("%f %f", &operand1,&operand2);
     /*Read the 2 numbers entered */
-    if(operand1==0 || operand==0)
+    if(operand1==0 || operand2==0)
     {
         printf("Both the numbers entered should be greater than 0");
     }
