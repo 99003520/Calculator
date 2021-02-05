e701a1aa291b467d13e1c74f5cb110ed9979cac2
99003520 <78562135+99003520@users.noreply.github.com>
2021-02-05 16:58:39 +0530

    Add files via upload

---
+++ per.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main()
+{
+    float a,b; 
+    float  percentage;
+
+    /* Input 2 numbers */
+    printf("Enter 2 numbers: \n");
+    scanf("%f %f", &a,&b);
+    /*Read the 2 numbers entered */
+    if(a==0 || b==0)
+    {
+        printf("Both the numbers entered should be greater than 0");
+    }
+    /*The numbers entered should be greater than 0 else error will be displayed in calculator*/
+    else
+    {
+
+    /* Calculate percentage */
+    /*Formula for percentage is */
+
+    percentage = (a / b )* 100;
+
+    /* Print the result */
+    printf("Percentage = %.2f", percentage);
+    /* The result is rounded off to 2 decimal places */
+    }
+    return 0;
+}
